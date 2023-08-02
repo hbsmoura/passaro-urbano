cab503d77f7464cdb31ef1c49c10d6997878d3fa
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-08-02 20:16:28 -0300

    services called on components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public ofertas: Array<Oferta> = []
+
+  constructor(private ofertasService: OfertasService) {}
+
+  ngOnInit(): void {
+    this.ofertas = this.ofertasService.getOfertas()
+  }
 
 }
---
+++ src/app/shared/oferta.model.ts
@@ -0,0 +1,18 @@
+export class Oferta {
+
+  constructor(
+    public id: number,
+    public categoria: string,
+    public titulo: string,
+    public descricao_oferta: string,
+    public anunciante: string,
+    public valor: number,
+    public destaque: boolean,
+    public imagens: Array<Imagem>
+  ){}
+
+}
+
+interface Imagem {
+  url: string
+}
